303384a6688445bcf130354c8e2a91ca75ed3cc1
Matthias Gatto <matthias.gatto@outscale.com>
2020-07-03 14:50:06 +0200

    docs: add v4 signature

---
+++ docs/cmdline-opts/v4-signature.d
@@ -0,0 +1,7 @@
+Long: v4-signature
+Arg: <provider1[:provider2]>
+Help: Performe V4 signature on HTTP header
+Added: 7.72.0
+---
+Performe V4 signature on HTTP header
+The provider argument is a string that is merged to some generated strings that are then hashed or add to the header
